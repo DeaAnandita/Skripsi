f42c7cf388e8443ab5d9e87dfa7a279e9459701e
belani11 <belanityas@gmail.com>
2025-09-29 15:44:18 +0700

    coba

---
+++ app/Http/Controllers/KesejahteraankeluargaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\kesejahteraankeluarga;
+use Illuminate\Http\Request;
+
+class KesejahteraankeluargaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(kesejahteraankeluarga $kesejahteraankeluarga)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(kesejahteraankeluarga $kesejahteraankeluarga)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, kesejahteraankeluarga $kesejahteraankeluarga)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(kesejahteraankeluarga $kesejahteraankeluarga)
+    {
+        //
+    }
+}
